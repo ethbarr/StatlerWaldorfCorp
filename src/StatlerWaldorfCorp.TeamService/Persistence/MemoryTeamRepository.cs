26b2f8bb02811edefe20bde14faed62ed4e3e0f3
Ethan Barrett <ethan.jh.barrett@gmail.com>
2019-02-14 08:58:58 -0500

    integration tests

--- src/StatlerWaldorfCorp.TeamService/Persistence/MemoryTeamRepository.cs
+++ src/StatlerWaldorfCorp.TeamService/Persistence/MemoryTeamRepository.cs
@@ -1,31 +1,91 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using StatlerWaldorfCorp.TeamService;
 using StatlerWaldorfCorp.TeamService.Models;
 
 namespace StatlerWaldorfCorp.TeamService.Persistence
 {
-    public class MemoryTeamRepository : ITeamRepository 
-    {
-        protected static ICollection<Team> teams;
+	public class MemoryTeamRepository :  ITeamRepository {
+		protected static ICollection<Team> teams;
 
-        public MemoryTeamRepository() 
-        {
-            if(teams == null) 
-                teams = new List<Team>(); 
-        }
+		public MemoryTeamRepository() {
+			if(teams == null) {
+				teams = new List<Team>();
+			}
+		}
 
-        public MemoryTeamRepository(ICollection<Team> teams) 
-        {
+		public MemoryTeamRepository(ICollection<Team> teams) {
             MemoryTeamRepository.teams = teams;
-        }
+		}
 
-        public IEnumerable<Team> GetTeams() 
-        {
-            return teams; 
-        }
+		public IEnumerable<Team> List() {
+			return teams; 
+		}
 
-        public void AddTeam(Team t) 
-        {
-            teams.Add(t);
-        }
-    }
+		public Team Get(Guid id) {
+			return teams.FirstOrDefault(t => t.ID == id);			
+		}
+
+		public Team Update(Team t) 
+		{
+			Team team = this.Delete(t.ID);
+	
+			if(team != null) {
+				team = this.Add(t);
+			}
+
+			return team;
+		}
+
+		public Team Add(Team team) 
+		{
+			teams.Add(team);
+			return team;
+		}
+
+		public Team Delete(Guid id) {	
+			var q = teams.Where(t => t.ID == id);
+			Team team = null;
+
+			if (q.Count() > 0) {				
+				team = q.First();
+				teams.Remove(team);
+			}							
+
+			return team; 			
+		}
+	}
 }
+
+// using System.Collections.Generic;
+// using StatlerWaldorfCorp.TeamService.Models;
+
+// namespace StatlerWaldorfCorp.TeamService.Persistence
+// {
+//     public class MemoryTeamRepository : ITeamRepository 
+//     {
+//         protected static ICollection<Team> teams;
+
+//         public MemoryTeamRepository() 
+//         {
+//             if(teams == null) 
+//                 teams = new List<Team>(); 
+//         }
+
+//         public MemoryTeamRepository(ICollection<Team> teams) 
+//         {
+//             MemoryTeamRepository.teams = teams;
+//         }
+
+//         public IEnumerable<Team> GetTeams() 
+//         {
+//             return teams; 
+//         }
+
+//         public void AddTeam(Team t) 
+//         {
+//             teams.Add(t);
+//         }
+//     }
+// }
--- test/StatlerWaldorfCorp.TeamService.Tests/TeamsControllerTest.cs
+++ test/StatlerWaldorfCorp.TeamService.Tests/TeamsControllerTest.cs
@@ -1,54 +1,196 @@
 using Xunit;
-using StatlerWaldorfCorp.TeamService.Models;
 using System.Collections.Generic;
-using System.Linq;
-using System;
-using StatlerWaldorfCorp.TeamService.Persistence;
+using StatlerWaldorfCorp.TeamService.Models;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
 
 namespace StatlerWaldorfCorp.TeamService
 {
     public class TeamsControllerTest
-    {
-
+    {	    
         [Fact]
         public void QueryTeamListReturnsCorrectTeams()
         {
-            ITeamRepository repo = new MemoryTeamRepository();
-            repo.AddTeam(new Team("one", Guid.NewGuid()));
-            repo.AddTeam(new Team("two", Guid.NewGuid()));
-            TeamsController controller = new TeamsController(new MemoryTeamRepository());
+            TeamsController controller = new TeamsController(new TestMemoryTeamRepository());
             var rawTeams = (IEnumerable<Team>)(controller.GetAllTeams() as ObjectResult).Value;
             List<Team> teams = new List<Team>(rawTeams);
-            Assert.Equal(2, teams.Count);
-            Assert.Equal("one", teams[0].Name);
-            Assert.Equal("two", teams[1].Name);  
+            Assert.Equal(teams.Count, 2);
+            Assert.Equal(teams[0].Name, "one");
+            Assert.Equal(teams[1].Name, "two");            
+        }
+
+        [Fact]
+        public void GetTeamRetrievesTeam() 
+        {
+            TeamsController controller = new TeamsController(new TestMemoryTeamRepository());
+
+            string sampleName = "sample";
+            Guid id = Guid.NewGuid();
+            Team sampleTeam = new Team(sampleName, id);
+            controller.CreateTeam(sampleTeam);
+
+            Team retrievedTeam = (Team)(controller.GetTeam(id) as ObjectResult).Value;
+            Assert.Equal(retrievedTeam.Name, sampleName);
+            Assert.Equal(retrievedTeam.ID, id);            
+        }
+
+        [Fact]
+        public void GetNonExistentTeamReturnsNotFound() 
+        {
+            TeamsController controller = new TeamsController(new TestMemoryTeamRepository());
+
+            Guid id = Guid.NewGuid();
+            var result = controller.GetTeam(id);
+            Assert.True(result is NotFoundResult);                                
         }
 
         [Fact]
         public void CreateTeamAddsTeamToList() 
         {
-            TeamsController controller = new TeamsController(new MemoryTeamRepository());
-            
-            var teams = (IEnumerable<Team>)
-                (controller.GetAllTeams() as ObjectResult).Value;
-            
+            TeamsController controller = new TeamsController(new TestMemoryTeamRepository());
+            var teams = (IEnumerable<Team>)(controller.GetAllTeams() as ObjectResult).Value;
             List<Team> original = new List<Team>(teams);
             
             Team t = new Team("sample");
             var result = controller.CreateTeam(t);
+            //TODO: also assert that the destination URL of the new team reflects the team's GUID
+            Assert.Equal((result as ObjectResult).StatusCode, 201);
 
-            var newTeamsRaw = 
-                (IEnumerable<Team>)
-                    (controller.GetAllTeams() as ObjectResult).Value;
-    
+            var actionResult = controller.GetAllTeams() as ObjectResult;            
+            var newTeamsRaw = (IEnumerable<Team>)(controller.GetAllTeams() as ObjectResult).Value;
             List<Team> newTeams = new List<Team>(newTeamsRaw);
-            Assert.Equal(original.Count+1, newTeams.Count);
-    
-            var sampleTeam = newTeams.FirstOrDefault( 
-                target => target.Name == "sample");
+            Assert.Equal(newTeams.Count, original.Count+1);
+            var sampleTeam = newTeams.FirstOrDefault( target => target.Name == "sample");
             Assert.NotNull(sampleTeam);            
-        } 
- 
+        }
+
+        [Fact]
+        public void UpdateTeamModifiesTeamToList() 
+        {
+            TeamsController controller = new TeamsController(new TestMemoryTeamRepository());
+            var teams = (IEnumerable<Team>)(controller.GetAllTeams() as ObjectResult).Value;
+            List<Team> original = new List<Team>(teams);
+            
+            Guid id = Guid.NewGuid();
+            Team t = new Team("sample", id);
+            var result = controller.CreateTeam(t);
+
+            Team newTeam = new Team("sample2", id);
+            controller.UpdateTeam(newTeam, id);
+
+            var newTeamsRaw = (IEnumerable<Team>)(controller.GetAllTeams() as ObjectResult).Value;
+            List<Team> newTeams = new List<Team>(newTeamsRaw);
+            var sampleTeam = newTeams.FirstOrDefault( target => target.Name == "sample");
+            Assert.Null(sampleTeam);
+
+            Team retrievedTeam = (Team)(controller.GetTeam(id) as ObjectResult).Value;
+            Assert.Equal(retrievedTeam.Name, "sample2");
+        }        
+
+        [Fact]
+        public void UpdateNonExistentTeamReturnsNotFound() 
+        {
+            TeamsController controller = new TeamsController(new TestMemoryTeamRepository());
+            var teams = (IEnumerable<Team>)(controller.GetAllTeams() as ObjectResult).Value;
+            List<Team> original = new List<Team>(teams);
+            
+            Team someTeam = new Team("Some Team", Guid.NewGuid());
+            controller.CreateTeam(someTeam);
+
+            Guid newTeamId = Guid.NewGuid();
+            Team newTeam = new Team("New Team", newTeamId);
+            var result = controller.UpdateTeam(newTeam, newTeamId);
+
+            Assert.True(result is NotFoundResult);
+        }
+
+        [Fact]
+        public void DeleteTeamRemovesFromList() 
+        {
+            TeamsController controller = new TeamsController(new TestMemoryTeamRepository());
+            var teams = (IEnumerable<Team>)(controller.GetAllTeams() as ObjectResult).Value;
+            int ct = teams.Count();
+
+            string sampleName = "sample";
+            Guid id = Guid.NewGuid();
+            Team sampleTeam = new Team(sampleName, id);
+            controller.CreateTeam(sampleTeam);
+
+            teams = (IEnumerable<Team>)(controller.GetAllTeams() as ObjectResult).Value;
+            sampleTeam = teams.FirstOrDefault(target => target.Name == sampleName);
+            Assert.NotNull(sampleTeam);            
+
+            controller.DeleteTeam(id);
+
+            teams = (IEnumerable<Team>)(controller.GetAllTeams() as ObjectResult).Value;
+            sampleTeam = teams.FirstOrDefault(target => target.Name == sampleName);
+            Assert.Null(sampleTeam);            
+        }
+
+        [Fact]
+        public void DeleteNonExistentTeamReturnsNotFound() 
+        {
+            TeamsController controller = new TeamsController(new TestMemoryTeamRepository());
+            Guid id = Guid.NewGuid();
+
+            var result = controller.DeleteTeam(id);
+            Assert.True(result is NotFoundResult);
+        }
     }
 }
+
+// using Xunit;
+// using StatlerWaldorfCorp.TeamService.Models;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System;
+// using StatlerWaldorfCorp.TeamService.Persistence;
+// using Microsoft.AspNetCore.Mvc;
+
+// namespace StatlerWaldorfCorp.TeamService
+// {
+//     public class TeamsControllerTest
+//     {
+
+//         [Fact]
+//         public void QueryTeamListReturnsCorrectTeams()
+//         {
+//             ITeamRepository repo = new MemoryTeamRepository();
+//             repo.AddTeam(new Team("one", Guid.NewGuid()));
+//             repo.AddTeam(new Team("two", Guid.NewGuid()));
+//             TeamsController controller = new TeamsController(new MemoryTeamRepository());
+//             var rawTeams = (IEnumerable<Team>)(controller.GetAllTeams() as ObjectResult).Value;
+//             List<Team> teams = new List<Team>(rawTeams);
+//             Assert.Equal(2, teams.Count);
+//             Assert.Equal("one", teams[0].Name);
+//             Assert.Equal("two", teams[1].Name);  
+//         }
+
+//         [Fact]
+//         public void CreateTeamAddsTeamToList() 
+//         {
+//             TeamsController controller = new TeamsController(new MemoryTeamRepository());
+            
+//             var teams = (IEnumerable<Team>)
+//                 (controller.GetAllTeams() as ObjectResult).Value;
+            
+//             List<Team> original = new List<Team>(teams);
+            
+//             Team t = new Team("sample");
+//             var result = controller.CreateTeam(t);
+
+//             var newTeamsRaw = 
+//                 (IEnumerable<Team>)
+//                     (controller.GetAllTeams() as ObjectResult).Value;
+    
+//             List<Team> newTeams = new List<Team>(newTeamsRaw);
+//             Assert.Equal(original.Count+1, newTeams.Count);
+    
+//             var sampleTeam = newTeams.FirstOrDefault( 
+//                 target => target.Name == "sample");
+//             Assert.NotNull(sampleTeam);            
+//         } 
+ 
+//     }
+// }
